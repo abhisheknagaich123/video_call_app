b5bc5e05fcff2b11cbf6569e3c8418d57444f686
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2022-12-31 09:11:39 +0530

    commit

---
+++ lib/utils.dart
@@ -0,0 +1,6 @@
+class Utils {
+
+  static int appId =800714486 ;
+  static String appSignin = "e1eaac14aba2876469ff3f0b9a2477b5820eafe0504a5948946108c8eeb08dab" ;
+
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import path_provider_macos
+import wakelock_macos
+import zego_express_engine
+import zego_zim
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+  ZegoExpressEnginePlugin.register(with: registry.registrar(forPlugin: "ZegoExpressEnginePlugin"))
+  ZegoZimPlugin.register(with: registry.registrar(forPlugin: "ZegoZimPlugin"))
 }
